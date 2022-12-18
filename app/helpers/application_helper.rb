c38f4776baf1ccb82763bb84c2f03fd4e8882e70
dubesoftware <therealdubesoftware@gmail.com>
2022-12-18 04:05:10 +0200

    Define render_flash_stream helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   include Pagy::Frontend
+  
+  def render_flash_stream
+    turbo_stream.update "flash", partial: "layouts/flash"
+  end
 
   def content_id
     "#{controller_name.underscore}_#{action_name}"
