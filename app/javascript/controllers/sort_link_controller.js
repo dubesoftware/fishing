7cfb37b788d24ac33716df44a2d81a08b9f5abc4
dubesoftware <therealdubesoftware@gmail.com>
2022-12-18 22:45:18 +0200

    Bind hidden fields to Stimulus targets

--- app/javascript/controllers/sort_link_controller.js
+++ app/javascript/controllers/sort_link_controller.js
@@ -2,9 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="sort-link"
 export default class extends Controller {
+	static targets = ["sort", "direction"]
+	
   updateForm(event) {
 		let searchParams = new URL(event.detail.url).searchParams
 		
-		// searchParams.get("sort")
+		this.sortTarget.value = searchParams.get("sort")
+		this.directionTarget.value = searchParams.get("direction")		
   }
 }
