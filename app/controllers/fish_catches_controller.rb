3466f0e9e6a0becac7fe52895bcb06bf97be24bb
dubesoftware <therealdubesoftware@gmail.com>
2022-12-18 04:38:53 +0200

    Revert "Render flash turbo stream when creating and updatingfish catches"
    This reverts commit 0f4dbf04bee1aed3f1d44efdfff4dce97d98a67a.

--- app/controllers/fish_catches_controller.rb
+++ app/controllers/fish_catches_controller.rb
@@ -20,7 +20,6 @@ class FishCatchesController < ApplicationController
   def update
     respond_to do |format|
       if @fish_catch.update(fish_catch_params)
-        flash.now[:notice] = "Catch successfully updated."
         format.turbo_stream do
           @fish_catches = fish_catches_for_bait(@fish_catch.bait)
         end
@@ -38,7 +37,6 @@ class FishCatchesController < ApplicationController
     
     respond_to do |format|
       if @fish_catch.save
-        flash.now[:notice] = "Catch successfully created."
         format.turbo_stream do
           @fish_catches = fish_catches_for_bait(@fish_catch.bait)
           @new_catch = current_user.fish_catches.new(bait: @fish_catch.bait)
@@ -52,7 +50,7 @@ class FishCatchesController < ApplicationController
 
   def destroy
     @fish_catch.destroy
-    flash.now[:notice] = "Catch successfully deleted."
+    flash.now[:notice] = "Catch successfully deleted"
     
     respond_to do |format|
       format.turbo_stream do
