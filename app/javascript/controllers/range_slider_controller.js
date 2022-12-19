247b62fa95d53c353237305ce97e6177031fa27f
dubesoftware <therealdubesoftware@gmail.com>
2022-12-19 02:37:59 +0200

    Connect Stimulus controller to target div

--- app/javascript/controllers/range_slider_controller.js
+++ app/javascript/controllers/range_slider_controller.js
@@ -2,6 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="range-slider"
 export default class extends Controller {
+	
+	static targets = ["slider"]
+
   connect() {
+		this.sliderTarget.innerHTML = "Put a slider here!"
   }
 }
