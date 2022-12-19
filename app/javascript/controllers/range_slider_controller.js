c3289db3f62afd70ddd530324c682136d6c2f338
dubesoftware <therealdubesoftware@gmail.com>
2022-12-19 02:56:57 +0200

    Create and destroy slider in connect and disconnect callbacks of Stimulus controller respectively

--- app/javascript/controllers/range_slider_controller.js
+++ app/javascript/controllers/range_slider_controller.js
@@ -1,11 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 
+import noUiSlider from 'nouislider'
+import wNumb from 'wnumb'
+
 // Connects to data-controller="range-slider"
 export default class extends Controller {
 	
 	static targets = ["slider"]
 
   connect() {
-		this.sliderTarget.innerHTML = "Put a slider here!"
+		this.slider = noUiSlider.create(this.sliderTarget, {
+			range: {
+				min: 0,
+				max: 10
+			},
+			start: [2, 8]
+		})
   }
+	
+	disconnect() {
+		this.slider.destroy()
+	}
 }
