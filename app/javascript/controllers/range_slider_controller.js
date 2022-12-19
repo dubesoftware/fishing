dc25bc540dc17f78874eee625c6f9a88bf349654
dubesoftware <therealdubesoftware@gmail.com>
2022-12-19 03:10:33 +0200

    Add slider options

--- app/javascript/controllers/range_slider_controller.js
+++ app/javascript/controllers/range_slider_controller.js
@@ -14,7 +14,10 @@ export default class extends Controller {
 				min: 0,
 				max: 10
 			},
-			start: [2, 8]
+			start: [2, 8],
+			step: 1,
+			connect: [false, true, false],
+			tooltips: [wNumb({decimals: 0}), wNumb({decimals: 0})]
 		})
   }
 	
