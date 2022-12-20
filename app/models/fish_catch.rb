386f4510b116ed76c965de25263196399e62f51b
dubesoftware <therealdubesoftware@gmail.com>
2022-12-20 23:28:56 +0200

    Revert "Broadcast new fish catch to activity channel"
    This reverts commit be0e41633c9ffc105b2d9ef0d82f7226041cd074.

--- app/models/fish_catch.rb
+++ app/models/fish_catch.rb
@@ -2,11 +2,6 @@ class FishCatch < ApplicationRecord
   belongs_to :bait
   belongs_to :user
   has_many :likes, dependent: :destroy
-  
-  after_create_commit -> {
-    broadcast_prepend_to "activity", target: catches,
-                         partial: "activity/fish_catch"
-  }
 
   SPECIES = [
     "Brown Trout",
