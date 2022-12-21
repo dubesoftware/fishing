5338c44daa65b2eb28b0c1509b017580bb907a08
dubesoftware <therealdubesoftware@gmail.com>
2022-12-22 00:19:27 +0200

    Revert "Use pagy with turbo frame to page through fish catch activity"
    This reverts commit cea1ff36dad39dddfbda6fb80b136a0d463a6b5f.

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -2,9 +2,7 @@ class ActivityController < ApplicationController
   before_action :require_signin
 
   def index
-    @pagy, @fish_catches =
-      pagy(FishCatch.order(created_at: :desc).includes(:user, :bait),
-        items: 5)
+    @fish_catches = FishCatch.order(created_at: :desc).includes(:user, :bait)
 
     @fish_catches = current_user.assign_my_likes_to_catches(@fish_catches)
   end
