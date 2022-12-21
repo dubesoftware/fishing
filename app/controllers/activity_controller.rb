93559f05025cbf6bddd1f36f17e684b442c5de53
dubesoftware <therealdubesoftware@gmail.com>
2022-12-21 23:23:03 +0200

    Limit catches to 5

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -2,7 +2,7 @@ class ActivityController < ApplicationController
   before_action :require_signin
 
   def index
-    @fish_catches = FishCatch.order(created_at: :desc).includes(:user, :bait)
+    @fish_catches = FishCatch.order(created_at: :desc).includes(:user, :bait).limit(5)
 
     @fish_catches = current_user.assign_my_likes_to_catches(@fish_catches)
   end
